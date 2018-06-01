91f9618df728b36950a1fa29d00ee79104f2e8eb
kmac1723 <37140839+kmac1723@users.noreply.github.com>
2018-06-01 18:28:52 +0100

    Initial commit for fork

---
+++ TODO.md
@@ -0,0 +1,4 @@
+Enable saving of data from bingocard into cookie. +++DONE
+
+Create a function to read data from local storage and initialise card from it.  ++++DONE
+Allow card to be filled from prevoius session data recorded in localStorage.++++DONE
--- script.js
+++ script.js
@@ -6,6 +6,31 @@
         return this.push.apply(this, rest);
     };
 
+    // function to check if web storage is avaialable (from developer.mozilla.org)
+    function storageAvailable(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch (e) {
+            return e instanceof DOMException && (
+                // everything except Firefox
+                e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === 'QuotaExceededError' ||
+                // Firefox
+                e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+                // acknowledge QuotaExceededError only if there's something already stored
+                storage.length !== 0;
+        }
+    };
+
         // We're going to be doing this in a mostly functional style as opposed to an
         // object-oriented one. Once the squares are assembled, the only relevant data about
         // them is whether they're clicked or not (which is best handled by a simple true/false
@@ -22,8 +47,16 @@
         // of course, one of the disadvantages of structuring things this way is that it's a bit
         // harder to make the control flow self-documenting. ^^;; So - clicking on a square triggers
         // fillSquare; fillSquare fires checkForWin; checkForWin sometimes fires "win."
+
+        // NEW STUFF: bingo card is now saved to browser local storage, which is then detected on 
+        // load.  Need to write another function to re-generate squares from saved data
+        // This will not be DRY, unless I add arguements to the generatrSquares function...
+        // I could also separate square generation and card creation, so the card is built the same way
+        // for both types of square generation.
         
     var grid = [[],[],[],[],[]],
+        // Create an array of sqare data objects that can contain info for saving to web storage.
+        squareData = [[],[],[],[],[]],
         winList,
 
         generateSquares = function (data) {
@@ -46,24 +79,67 @@
 
                 sectionTag = function (x, y, slug, content) {
                     return '<section class="square" id="' + x + '-' + y + '" data-slug="' + slug + '"><span>' + content + '</span></section>';
-                };
+                },
+                // Generate and save new squares
+                generateNewSquares = function(){
+                    eachGridElement(function (x, y) {
+                        if (x === 2 && y === 2) {
+                            grid[2][2] = true;
+                            // Add free square to squareData array.
+                            squareData[2][2] = {
+                                title: "free-space",
+                                comment: "An actually good, effective scene!",
+                                response: ""
+                            };
+                        } else {
+                            source = randomSource();
+                            // Set up the isTicked grid.
+                            grid[x][y] = false;
+                            // Add data to the squareData array.
+                            var newSquare = {
+                                title: source.attr('data-slug'),
+                                comment: source.find('.comment').html(),
+                                response: source.find('.retort').html()
+                            }
+                            squareData[x][y] = newSquare;
+                        }
+                    });
+                },
+                // Read data from passed data object into arrays.
+                readSquares = function(sqrData, grdData){
+                    // Read element data and create squares from it.
+                    eachGridElement(function(x, y){
+                        // Set up the isTicked grid.
+                        grid[x][y] = grdData[x][y];
+                        // Read data into sqauredData
+                        squareData[x][y] = sqrData[x][y];
+                    });
+                },               
+                createCard = function(){
+                    eachGridElement(function (x, y) {
+                        card.append(sectionTag(x, y, squareData[x][y].title, squareData[x][y].comment));
+                        // Check if the square is filled
+                        if(grid[x][y]){
+                            // Find the element based on the id tag.
+                            var sqr = $('#' + x + '-' + y);
+                            // Add filled class to element
+                            sqr.addClass('filled');
+                            // Add troll class to the matching element in winlist, which will be used to create the victory screen
+                            winList.find('[data-slug="' + sqr.attr('data-slug') + '"]').addClass("troll");
+                        }
+                        
 
-            eachGridElement(function (x, y) {
-                var source;
-                if (x === 2 && y === 2) {
-                    card.append(sectionTag(x, y, "free-space", "I would listen to you if you weren't so angry."));
-                    $('#2-2').addClass('free filled');
-                    grid[2][2] = true;
-                }
-                else {
-                    source = randomSource();
-                    card.append(sectionTag(x, y, source.attr('data-slug'), source.find('.comment').html()));
-                    grid[x][y] = false;
-                }
-            });
+                    });
+                };
+            if(data){
+                readSquares(data.squareData, data.filledData);
+            } else {
+                generateNewSquares();
+            } 
+            createCard();
         },
 
-        // maps the given function over a 5x5 set of indices
+        // Maps the given function over a 5x5 set of indices
         eachGridElement = function (block) {
             for (x = 0; x < 5; x++) {
                 for (y = 0; y < 5; y++) {
@@ -72,21 +148,14 @@
             }
         },
 
-        // a specific playlist is used so that we better ensure sure no triggery videos are accidentally returned by Google's search algorithm
-        getKittenVideo = function () {
-            var url = "https://gdata.youtube.com/feeds/api/playlists/PL2A2C191E3D22BAF4?v=2";
-            $.get(url, function (data) {
-                data = $(data);
-                entry = $(data.find('entry')[Math.floor(Math.random() * 19)]);
-                $('#video').attr('src', entry.find('content').attr('src'));
-            }, "xml");
-        },
-
+        // NOTE: This function is added to a click listener on each .square element.
         fillSquare = function (square) {
             var coords = square.attr('id').split('-');
             square.addClass('filled');
             winList.find('[data-slug="'+square.attr('data-slug')+'"]').addClass("troll");
             grid[coords[0]][coords[1]] = true;
+            // just reset the entire string.
+            localStorage.setItem('gridData', JSON.stringify(grid));
 
             checkForWin();
         },
@@ -167,16 +236,43 @@
         win = function () {
             $('body').addClass("won");
             $("html, body").animate({ scrollTop: 0 }, "fast");
+            // Clear all stored data
+            localStorage.clear();
         };
 
     $(document).ready(function () {
-        winList = $('.win ul');
-        generateSquares();
 
+        winList = $('.win ul');
+
+        // Check if localStorage is enabled
+        if (storageAvailable('localStorage')) {
+            console.log('Local Storage available!');
+            // Check to see if partially completed bingo card is stored.
+            if (localStorage.getItem('squareData') && localStorage.getItem('gridData')){
+                // Create squares from stored values
+                // Put into an object and then pass into generateSquares.
+                var dat = {
+                    squareData: JSON.parse(localStorage.getItem('squareData')),
+                    filledData: JSON.parse(localStorage.getItem('gridData'))
+                }
+                generateSquares(dat);
+            } else {
+                // Generate new squares
+                generateSquares();
+                // Store the generated square data in local storage
+                localStorage.setItem('squareData', JSON.stringify(squareData));
+                // Store grid of filled squares in local storage.
+                localStorage.setItem('gridData', JSON.stringify(grid));
+            } 
+        }
+        else {
+            console.log('Local storage is not available, too bad...');
+            // Generate new squares, but don't bother saving to unavailable storage
+            generateSquares();
+        }
+        // Add click listeners to all .square classes.
         $('.square').click(function () {
             fillSquare($(this));
         });
-
-        getKittenVideo();
     });
 })(jQuery);
